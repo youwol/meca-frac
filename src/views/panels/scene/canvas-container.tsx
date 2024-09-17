c3e4cc16b98b2b7f3715712437b132d80926b700
jehadmelad <jd.melad@gmail.com>
2024-09-17 14:16:12 +0200

    ✨ canvas-container.tsx: select objects

--- src/views/panels/scene/canvas-container.tsx
+++ src/views/panels/scene/canvas-container.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   BufferAttribute,
   BufferGeometry,
@@ -6,9 +6,11 @@ import {
   DoubleSide,
   Mesh,
   MeshBasicMaterial,
+  Raycaster,
   TypedArray,
+  Vector2,
 } from "three";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
   Center,
   Environment,
@@ -26,7 +28,7 @@ export function CanvasContainer() {
   const { dataframeValue } = useDataFrameContext();
 
   const meshes = meshesArray(dataframeValue);
-  const renderMesh = meshRenderer({ meshes });
+
   return (
     <div id={"canvas-container"} className={"w-100 h-100 "}>
       <Canvas
@@ -43,13 +45,10 @@ export function CanvasContainer() {
           near={0.0001}
           far={10000}
         />
-        <group>
-          <Center>{renderMesh.map((m) => m)}</Center>
-        </group>
+        <ClickableMeshGroup meshes={meshes} />
         <gridHelper scale={100} args={[30, 30, "red", "gray"]} />
         <TrackballControls />
         <Environment preset="studio" />
-
         <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
           <GizmoViewport
             axisColors={["#9d4b4b", "#2f7f4f", "#3b5b9d"]}
@@ -61,13 +60,64 @@ export function CanvasContainer() {
   );
 }
 
+function ClickableMeshGroup({ meshes }: { meshes: Mesh[] }) {
+  const { camera } = useThree();
+  const { color } = useNodeColorContext();
+
+  const raycaster = useRef(new Raycaster());
+  const mouse = useRef(new Vector2());
+
+  const handleClick = (event: MouseEvent) => {
+    // Convert the mouse position to normalized device coordinates (-1 to 1 range)
+    const canvas = event.target as HTMLCanvasElement;
+    const bounds = canvas.getBoundingClientRect();
+    mouse.current.x =
+      ((event.clientX - bounds.left) / canvas.clientWidth) * 2 - 1;
+    mouse.current.y =
+      -((event.clientY - bounds.top) / canvas.clientHeight) * 2 + 1;
+
+    raycaster.current.setFromCamera(mouse.current, camera);
+    const intersects = raycaster.current.intersectObjects(meshes);
+
+    if (intersects.length > 0) {
+      const selectedMesh = intersects[0].object as Mesh;
+      const selectedMesh1 = selectedMesh.material as MeshBasicMaterial;
+      selectedMesh1.wireframe = !selectedMesh1.wireframe;
+      selectedMesh1.color.setRGB(
+        color.rgb.r / 255,
+        color.rgb.g / 255,
+        color.rgb.b / 255,
+      );
+      selectedMesh1.visible = !selectedMesh1.visible;
+      console.log(
+        "Selected Material attributes:",
+        selectedMesh.material as MeshBasicMaterial,
+      );
+    }
+  };
+
+  useFrame(({ gl }) => {
+    gl.domElement.addEventListener("click", handleClick);
+    return () => {
+      gl.domElement.removeEventListener("click", handleClick);
+    };
+  });
+
+  return (
+    <Center>
+      {meshes.map((m) => (
+        <primitive key={m.id} object={m} scale={0.01} />
+      ))}
+    </Center>
+  );
+}
+
 function meshesArray(dataframeValue: DataFrame[]) {
   return dataframeValue.map((dataframe) => {
     const vertices = dataframe.series.positions.array as TypedArray;
     const indices = dataframe.series.indices.array as TypedArray;
 
     const geom = new BufferGeometry();
-
     geom.setIndex(new BufferAttribute(indices, 1));
     geom.setAttribute("position", new BufferAttribute(vertices, 3));
 
@@ -79,12 +129,6 @@ function meshesArray(dataframeValue: DataFrame[]) {
 
     return new Mesh(geom, material);
   });
-}
-
-function meshRenderer({ meshes }: { meshes: Mesh[] }) {
-  return meshes.map((mesh) => (
-    <primitive key={mesh.id} object={mesh} scale={0.01} position={[0, 0, 0]} />
-  ));
 }
 
 const randColor = () => {
